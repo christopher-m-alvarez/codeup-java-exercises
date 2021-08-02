133a70b720e1d36652aae6116cfe49d6fd437093
Christopher Alvarez <christopher.m.alvarez93@gmail.com>
2021-08-02 11:50:55 -0500

    Syntax, Types, and Variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,26 @@
 public class HelloWorld {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+
+//        int myFavoriteNumber = 8;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "3.14159";
+//        System.out.println(myString);
+//
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int y = 5;
+//        System.out.println(++y);
+//        System.out.println(y);
+
+
     }
-
-
-    int myFavoriteNumber = 8;
-
-
 }
