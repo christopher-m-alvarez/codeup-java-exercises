9c9d7d6e1871de6c3951b9b45d0144b7cff7cd1f
Christopher Alvarez <christopher.m.alvarez93@gmail.com>
2021-08-03 13:43:00 -0500

    strings exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -7,6 +7,14 @@ public class StringExercise {
 //        System.out.println(message);
 //        System.out.println(message.replace("education", "thought control"));
 
+//        String message = "Check \"this\" out!, \"s inside of \"s!";
+//        System.out.println(message);
 
+//        String message = "In windows, the main drive is usually C:\\";
+//        System.out.println(message);
+
+//        String message = "I can do backslashes \\, double backslashes \\\\,\n" +
+//                "and the amazing triple backslash \\\\\\!";
+//        System.out.println(message);
     }
 }
