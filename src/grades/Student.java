0ec81100f8e61ccb001fc349e7feae3962b88f6b
Christopher Alvarez <christopher.m.alvarez93@gmail.com>
2021-08-12 16:24:22 -0500

    array list

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,4 +1,14 @@
 package grades;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 
 public class Student {
+    private String studentName;
+    private ArrayList<Integer> grades;
+
+    public Student(String studentName, ArrayList<Integer> grades) {
+        this.studentName = studentName;
+        this.grades = new ArrayList<>;
+    }
 }
